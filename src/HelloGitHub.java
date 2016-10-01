0764956f7c6f0b07a19b1b6804ee0b4f9f074a6f
EKrenn <elisabeth.krenn@edu.campus02.at>
2016-10-01 11:41:45 +0200

    Demo

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -2,6 +2,9 @@
 public class HelloGitHub {
 
 	public static void main(String[] args) {
+		System.out.println(56);
+		System.out.println(5.6);
+		System.out.println(true);
 		System.out.println("Hello GitHub von mir 2");
 
 	}
