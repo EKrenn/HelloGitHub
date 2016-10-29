7112cfebc58386f18bd11240573ad69c63fa3848
EKrenn <elisabeth.krenn@edu.campus02.at>
2016-10-29 14:38:39 +0200

    Classe gelöscht

--- src/OperatorsArithmetic.java
+++ src/OperatorsArithmetic.java
@@ -1,14 +1,18 @@
 
 public class OperatorsArithmetic {
-	public static double celsius2Fahrenheit(double celsius) {
-		double fahrenheit = 9.0/5 * celsius + 32;
-		return fahrenheit;
-		
-	}
+	
 	
 	public static void main(String[] args) {
 		double tempF = celsius2Fahrenheit(26.0);
 		System.out.println("Die heutige Temperatur ist in Fahrenheit: "+tempF);
+		
+		String result = printRectangleArea("Die Fl√§che ist: ", 3, 4);
+		System.out.println(result);
+		
+		double tempF = celsius2Fahrenheit(26.0);
+		System.out.println("Fahrenheit: "+tempF);
+		double tempC = fahrenheit2Celsius(72.5);
+		System.out.println("Celsius: "+tempC);
 		/*
 		// ‹bung 1
 		
@@ -30,6 +34,22 @@ public class OperatorsArithmetic {
 		 	*/	
 	}
 	
+	
+	public static String printRectangleArea(String text, int a, int b)
+	{
+		int ergebnis = a*b;
+		return text+" "+ergebnis; 
+	}
+	public static double celsius2Fahrenheit(double celsius)
+	{
+		double fahrenheit = (9.0/5) * celsius + 32;
+		return fahrenheit;
+	}
+	public static double fahrenheit2Celsius(double fahrenheit)
+	{
+		double celsius = (fahrenheit - 32) / (9.0/5);
+		return celsius;
+	}
 	
 	
 
