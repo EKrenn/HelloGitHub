8f602877f51249bf6cece30a2425b28cb68adca0
EKrenn <elisabeth.krenn@edu.campus02.at>
2016-10-29 14:33:18 +0200

    Test Commit

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -3,14 +3,9 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 
-		int zahl = 20;
-		boolean isSunny = true;
-		
-		System.out.println(zahl);
-		System.out.println(5.6);
-		System.out.println(isSunny);
-		System.out.println("Hello GitHub von mir 2");
-		
+		int test =0;
+		int test1 =0;
+		int test2 =0;
 		
 
 	}
