d54a3b6d5406a5d9ce13c4b9721e9ff0c0196920
EKrenn <elisabeth.krenn@edu.campus02.at>
2016-11-19 09:49:27 +0100

    Übung: Konto und Kontoklasse

--- Klassen/Konto.java
+++ Klassen/Konto.java
@@ -1,33 +1,4 @@
 
 public class Konto {
-	private String kontoinhaber;
-	private double konstostand;
-	
-	public void setInhaber(String inhaber)
-	{
-		Konto konto = new Konto();
-		konto.kontoinhaber = inhaber;
-		konto.konstostand = 0.0;
-	}
-	public void aufbuchen(double bebuchterWert)
-	{
-		Konto konto = new Konto();
-		double neuerWert = konto.konstostand + bebuchterWert;
-		System.out.println(neuerWert);
-	}
-	public void abbuchen(double abgebuchterWert)
-	{
-		Konto konto = new Konto();
-		double neuerWert = konto.konstostand - abgebuchterWert;
-		System.out.println(neuerWert);
-		if (neuerWert < 0)
-		{
-			System.out.println("Kontostand unterschritten - Geht leider nicht!");
-		}
-		
-		
-	}
+
 }
-
-
-
--- Klassen/Kontoausgabe.java
+++ Klassen/Kontoausgabe.java
@@ -2,10 +2,8 @@
 public class Kontoausgabe {
 
 	public static void main(String[] args) {
-		Konto ausgabe = new Konto();
-		ausgabe.setInhaber("Max Mustermann");
-		ausgabe.aufbuchen(234.50);
-		ausgabe.abbuchen(34.50);
+		// TODO Auto-generated method stub
+
 	}
 
 }
