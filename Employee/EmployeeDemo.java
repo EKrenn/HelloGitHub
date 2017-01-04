7e74a159cdd005c028b8f16a7e645a635e05ccc3
EKrenn <elisabeth.krenn@edu.campus02.at>
2017-01-04 11:04:58 +0100

    BlackJack

--- Employee/EmployeeDemo.java
+++ Employee/EmployeeDemo.java
@@ -5,6 +5,7 @@ public class EmployeeDemo {
 		EmployeeManager emp = new EmployeeManager();
 		Employee emp1 = new Employee(1, "Max Muster", 2000.0, "IT");
 		emp.addEmployee(emp1);
+		emp.findByDepartment("IT");
 		
 	}
 
